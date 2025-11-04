1282075040e665fb9443dfbf2f210679213d41c8
ValkGT <lencesmartin02@gmail.com>
2025-11-04 11:41:22 +0100

    pridaná landing page pre frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,16 +4,19 @@ import LoginView from '../views/LoginView.vue'
 import RegisterChoiceView from '../views/RegisterChoiceView.vue'
 import RegisterStudentView from '../views/RegisterStudentView.vue'
 import RegisterCompanyView from '../views/RegisterCompanyView.vue'
+import LandingPage from '../views/LandingPage.vue'
+
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
+    { path: '/home', name: 'home', component: HomeView },
     { path: '/login', name: 'login', component: LoginView },
     { path: '/register', name: 'registerChoice', component: RegisterChoiceView },
     { path: '/register/student', name: 'registerStudent', component: RegisterStudentView },
     { path: '/register/company', name: 'registerCompany', component: RegisterCompanyView },
+    { path: '/', name: 'landing', component: LandingPage },
   ],
 })
 
